b7a31bbda7debb23599764a8983e45f3e3531761
JustinMassart <justin.massart@student.hepl.be>
2023-04-29 17:23:25 +0200

    Added Pest & tests

---
+++ tests/Feature/Beers/BeerControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+
+Here, you can create tests for the BeerController. Meaning a test for each operation that the controller can do.
+
+To create a test, please use the ` it()` function like the following :
+
+it('ensures that the /beers route retreive data', function () {});
+
+*/
+
+
+it('ensures that the /beers route is accessible and returns data', function () {
+    $response = $this->get('/fr/beers');
+    $response->assertStatus(200);
+
+    $response_content = json_decode($response->getContent());
+
+    expect($response_content->beers->total)->toBeGreaterThanOrEqual(1);
+});
