2a981a23144815a714a9df7117ead9d790903eb5
JustinMassart <justin.massart@student.hepl.be>
2023-06-13 15:34:53 +0200

    Added home route & HomeController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BeerController;
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\PlaceController;
@@ -19,6 +20,11 @@ use Illuminate\Support\Facades\Route;
 /* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 }); */
+
+// Home
+
+Route::get('/', [HomeController::class, 'index'])->middleware('setLocale');
+Route::get('/{locale?}/', [HomeController::class, 'index'])->middleware('setLocale');
 
 // Beers
 
