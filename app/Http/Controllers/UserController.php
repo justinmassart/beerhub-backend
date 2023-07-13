bb9acc40ebfbadd1ac17c8a6c8baf3bc04c0c8f7
JustinMassart <justin.massart@student.hepl.be>
2023-07-13 16:33:10 +0200

    Added store function to register user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+use App\Http\Requests\SignUpFormRequest;
+use App\Models\Role;
+
 class UserController extends Controller
 {
     /**
@@ -25,9 +29,39 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(SignUpFormRequest $request)
     {
-        //
+        $validated = $request->safe()->only(
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'country',
+            'password',
+            'confirm_password'
+        );
+
+        try {
+            if ($validated) {
+                $user = User::create([
+                    'first_name' => $validated['first_name'],
+                    'last_name' => $validated['last_name'],
+                    'username' => $validated['userename'],
+                    'email' => $validated['email'],
+                    'country' => $validated['country'],
+                    'password' => bcrypt($validated['password']),
+                ]);
+
+                Role::create([
+                    'access_rights' => 'user',
+                    'user_id' => $user->id,
+                ]);
+
+                return response()->json(['success' => 'ACCOUNT_CREATED', 'user' => [$user->first_name]]);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'FAILED_TO_CREATE_USER']);
+        }
     }
 
     /**
