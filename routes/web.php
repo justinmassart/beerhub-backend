6c1487022972683b9b26c729a326a89a70252865
JustinMassart <justin.massart@student.hepl.be>
2023-07-19 16:15:26 +0200

    Added Breeze & Fixed Vite + Valet & Added tailwind

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,22 +28,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
-
-// Email Verification
-
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
-
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-
-    return redirect('/');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+require __DIR__.'/auth.php';
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,37 @@
-import { defineConfig } from 'vite';
+import fs from 'fs';
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+import { homedir } from 'os';
+import { resolve } from 'path';
+
+let host = 'beerhub.test';
 
 export default defineConfig({
-    server: {
-        host: '192.168.129.4',
-    },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-            valetTls: 'beerhub.test',
-        }),
-    ],
+    plugins: [laravel(['resources/js/app.js', 'resources/css/app.css'])],
+    server: detectServerConfig(host),
 });
+
+function detectServerConfig(host) {
+    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`);
+    let certificatePath = resolve(
+        homedir(),
+        `.config/valet/Certificates/${host}.crt`,
+    );
+
+    if (!fs.existsSync(keyPath)) {
+        return {};
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return {};
+    }
+
+    return {
+        hmr: { host },
+        host,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    };
+}
