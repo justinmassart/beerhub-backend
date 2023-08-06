f284935f89f26e8475160ad315a3ef3b56e695ed
JustinMassart <justin.massart@student.hepl.be>
2023-08-06 15:44:44 +0200

    Added self user factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ use App\Models\PlaceRating;
 use Illuminate\Database\Seeder;
 use App\Models\BeerRatingTotal;
 use App\Models\PlaceRatingTotal;
+use App\Models\User;
+use App\Models\UserPreference;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,10 +22,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::factory(100)->create();
-        BeerBrand::factory(250)->create();
+        Role::factory(50)->create();
+        BeerBrand::factory(100)->create();
         BeerPlace::factory(100)->create();
-        BeerRating::factory(1000)->create();
-        PlaceRating::factory(1000)->create();
+        BeerRating::factory(250)->create();
+        PlaceRating::factory(250)->create();
+
+        $user = User::create([
+            'firstname' => 'Justin',
+            'lastname' => 'Massart',
+            'username' => 'TheDev',
+            'email' => 'justinmassart@outlook.com',
+            'phone' => '+32494391109',
+            'phone_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'DOB' => '2000-11-09',
+            'country' => 'BE',
+            'user_preferences_id' => UserPreference::factory()->create()->id,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Role::create([
+            'access_rights' => 'user',
+            'user_id' => $user->id,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
