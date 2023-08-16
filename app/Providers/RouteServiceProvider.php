a553ea0eca5c98c615b579a474b7c32501cefb0a
JustinMassart <justin.massart@student.hepl.be>
2023-08-16 15:05:18 +0200

    Updated query rate limiter for inputs routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,12 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware(['api', 'throttle:120,1'])
+                ->prefix('api')
+                ->group(function () {
+                    Route::get('/search-inputs/beer-color-selector', [SearchInputsController::class, 'beer_colors']);
+                });
         });
     }
 }
